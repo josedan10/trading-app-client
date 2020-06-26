5431491b68e8af20747b920110c31e610d3941fc
José Daniel  Quintero <josedanq100@gmail.com>
2020-06-26 10:55:20 -0400

    fix: preparing deploy

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   env: {
     SERVER_URL: 'https://mysterious-brook-83261.herokuapp.com/',
     TEST_SERVER_URL: 'http://localhost:4000',
-    APP_ENV: 'debug',
+    APP_ENV: 'deploy',
   },
 }
